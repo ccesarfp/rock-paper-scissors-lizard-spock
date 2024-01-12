ff6965957574653d5e905f139abd7ce73f19c8be
Caio Padilha <108874158+ccesarfp@users.noreply.github.com>
2024-01-12 16:23:12 +0000

    Preparing vue project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+import { RouterView } from 'vue-router';
+
+<template>
+  <div id="app">
+    <div id="nav" />
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss">
+  @import './styles/reset.scss';
+
+  #app {
+    font-family: 'Barlow Semi Condensed', Helvetica, sans-serif;
+  }
+</style>
